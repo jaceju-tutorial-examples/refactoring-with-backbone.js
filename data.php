d95b8fe686aaa7afdf2d00a996f1f1b6583e6d2a
Jace Ju <jaceju@gmail.com>
2012-05-03 15:42:55 +0800

    fix data structure

--- data.php
+++ data.php
@@ -1,29 +1,34 @@
 <?php
 
 return array(
-  1 => array(
+  array(
     'id' => 1,
     'name' => 'Product 1',
     'image' => 'img/product_1.jpg',
+    'desc' => 'Nullam tincidunt malesuada vehicula. Maecenas nibh metus, euismod nec laoreet et, placerat eu nibh. Nullam vulputate tristique ligula sed rutrum. Vestibulum pharetra sagittis est sit amet vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   ),
-  2 => array(
+  array(
     'id' => 2,
     'name' => 'Product 2',
     'image' => 'img/product_2.jpg',
+    'desc' => 'Nullam vulputate tristique ligula sed rutrum. Vestibulum pharetra sagittis est sit amet vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt malesuada vehicula. Maecenas nibh metus, euismod nec laoreet et, placerat eu nibh.',
   ),
-  3 => array(
+  array(
     'id' => 3,
     'name' => 'Product 3',
     'image' => 'img/product_3.jpg',
+    'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt malesuada vehicula. Maecenas nibh metus, euismod nec laoreet et, placerat eu nibh. Nullam vulputate tristique ligula sed rutrum. Vestibulum pharetra sagittis est sit amet vehicula.',
   ),
-  4 => array(
+  array(
     'id' => 4,
     'name' => 'Product 4',
     'image' => 'img/product_4.jpg',
+    'desc' => ' Vestibulum pharetra sagittis est sit amet vehicula.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vulputate tristique ligula sed rutrum. Nullam tincidunt malesuada vehicula. Maecenas nibh metus, euismod nec laoreet et, placerat eu nibh.',
   ),
-  5 => array(
+  array(
     'id' => 5,
     'name' => 'Product 5',
     'image' => 'img/product_5.jpg',
+    'desc' => 'Nullam tincidunt malesuada vehicula. Maecenas nibh metus, euismod nec laoreet et, placerat eu nibh. Nullam vulputate tristique ligula sed rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pharetra sagittis est sit amet vehicula.',
   ),
 );
--- json.php
+++ json.php
@@ -2,9 +2,5 @@
 
 $data = include('data.php');
 
-$id = isset($_GET['id']) ? (int) $_GET['id'] : null;
-
-$result = isset($data[$id]) ? $data[$id] : $data;
-
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode($result);
+echo json_encode($data);
