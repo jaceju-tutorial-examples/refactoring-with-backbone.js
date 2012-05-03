20420ae0b535645cefd979146aa6c85db21de258
Jace Ju <jaceju@gmail.com>
2012-05-03 17:49:19 +0800

    introduce item view

---
+++ js/app_5.js
@@ -0,0 +1,64 @@
+var Product = Backbone.Model.extend({
+  defaults: {
+    name: '',
+    image: '',
+    desc: ''
+  }
+});
+
+var ProductList = Backbone.Collection.extend({
+  model: Product
+});
+
+var ProductItemView = Backbone.View.extend({
+  template: _.template($('#item-template').html()),
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+var ProductInfoView = Backbone.View.extend({
+  template: _.template($('#info-template').html()),
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+var init = function () {
+  $.get('json.php', function (json) {
+
+    var productList = new ProductList(json);
+
+    $ul = $('#product-list>ul').empty();
+    $info = $('#product-info');
+
+    productList.forEach(function (product) {
+      var itemView = new ProductItemView({
+        model: product
+      })
+      $ul.append(itemView.render().el);
+    });
+
+    var infoView = new ProductInfoView({
+      el: '#product-info'
+    });
+    $('a', $ul).bind('click', function (e) {
+      e.preventDefault();
+      var id = this.id.replace(/^product-/, '');
+      var product = productList.get(id);
+
+      $('a', $ul).removeClass('active')
+      $(this).addClass('active');
+
+      infoView.model = product;
+      infoView.render();
+    }).eq(0).click();
+  }, 'json');
+};
+
+
+$(function () {
+  init();
+});
