e223eabc3f23f1e4b2891e979aad5b29c5415d2b
Jace Ju <jaceju@gmail.com>
2012-05-03 16:50:15 +0800

    version 2 - replace global variables with model

---
+++ js/app_2.js
@@ -0,0 +1,47 @@
+var Product = Backbone.Model.extend({
+  defaults: {
+    name: '',
+    image: '',
+    desc: ''
+  }
+});
+
+var ProductList = Backbone.Collection.extend({
+  model: Product
+});
+
+var init = function () {
+  $.get('json.php', function (json) {
+
+    var productList = new ProductList(json);
+
+    $ul = $('#product-list>ul').empty();
+    $info = $('#product-info');
+
+    productList.forEach(function (product) {
+      $ul.append('<li><a id="product-'
+        + product.get('id')
+        + '" href="#">' + product.get('name')
+        + '</a></li>');
+    });
+    $('a', $ul).bind('click', function (e) {
+      e.preventDefault();
+      var id = this.id.replace(/^product-/, '');
+      var product = productList.get(id);
+
+      $('a', $ul).removeClass('active')
+      $(this).addClass('active');
+      $('h1', $info).text(product.get('name'));
+      $('img', $info).attr({
+        src: product.get('image'),
+        alt: product.get('name')
+      });
+      $('.desc', $info).text(product.get('desc'));
+    }).eq(0).click();
+  }, 'json');
+};
+
+
+$(function () {
+  init();
+});
