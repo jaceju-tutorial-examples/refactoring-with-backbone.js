707e76319d92db213dbe9baabf4e4c8810596918
Jace Ju <jaceju@gmail.com>
2012-05-03 20:41:51 +0800

    introduce collection/model

--- js/app.js
+++ js/app.js
@@ -1,38 +1,47 @@
-var productList = null;
+var Product = Backbone.Model.extend({
+  defaults: {
+    name: '',
+    image: '',
+    desc: ''
+  }
+});
+
+var ProductList = Backbone.Collection.extend({
+  model: Product
+});
 
 var init = function () {
 
   $.get('json.php', function (json) {
 
-    productList = json;
+    var productList = new ProductList(json);
+
     var $ul = $('#product-list>ul').empty();
     var $info = $('#product-info');
 
-    for(idx in productList) {
-      var product = productList[idx];
+    productList.forEach(function (product) {
       $ul.append('<li><a id="product-'
-        + product.id
-        + '" href="#">' + product.name
+        + product.get('id')
+        + '" href="#">' + product.get('name')
         + '</a></li>');
-    }
+    });
 
     $('a', $ul).bind('click', function (e) {
 
       e.preventDefault();
 
       var id = this.id.replace(/^product-/, '');
-      var product = _.find(productList, function(p) {
-        return p.id == id;
-      });
+      var product = productList.get(id);
 
       $('a', $ul).removeClass('active')
       $(this).addClass('active');
-      $('h1', $info).text(product.name);
+      $('h1', $info).text(product.get('name'));
       $('img', $info).attr({
-        src: product.image,
-        alt: product.name
+        src: product.get('image'),
+        alt: product.get('name')
       });
-      $('.desc', $info).text(product.desc);
+
+      $('.desc', $info).text(product.get('desc'));
 
     }).eq(0).click();
 
